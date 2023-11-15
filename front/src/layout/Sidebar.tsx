ce047aed740fdb9053eb3cb62ec86e18e092d521
pl <pllacan.cours@gmail.com>
2023-11-15 15:29:28 +0100

    sidebar

--- front/src/layout/Sidebar.tsx
+++ front/src/layout/Sidebar.tsx
@@ -45,106 +45,109 @@ export default function Sidebar({
     setFilters(selectedFilters.filter((item) => item !== ""));
   }, [state]);
   return (
-      <form className={`${
+      <form className={`p-2 w-60 h-screen fixed bg-white -mt-3 overflow-scroll `}>
+        <h2 className="flex items-center justify-center mb-4">
+          <img src={filters} alt="" className="!w-4 mr-4" />
+          Filtres
+        </h2>
+        <div className={`${
       state.props.rotate90 ? "" : "pr-4"
-        } p-2 w-60 h-screen fixed bg-white -mt-3 overflow-scroll`}>
-      <h2 className="flex items-center justify-center mb-4">
-        <img src={filters} alt="" className="!w-4 mr-4" />
-        Filtres
-      </h2>
-      <div className="hover:bg-primary-hover hover:rounded py-1 ">
-        <label htmlFor="reset" className="flex items-center cursor-pointer">
-          <img src={trash} alt="" className="!w-4 mr-4" />
-          Effacer les filtres
-        </label>
-        <input
-          type="reset"
-          name="reset"
-          id="reset"
-          hidden
-          onClick={() => tags.map((tag) => dispatch({ [tag.name]: false }))}
-        />
-      </div>
-      <div className="mt-4">
-        <span
-          className="flex items-center justify-between cursor-pointer"
-          onClick={() =>
-            dispatch({ props: { rotate90: !state.props.rotate90 } })
-          }
-        >
-          Spécialités
-          <img
-            src={arrow}
-            alt=""
-            className={`w-2 transition ${
-              state.props.rotate90 ? "-rotate-90" : ""
-            }`}
-          />
-        </span>
-        <div
-          className={`${
-            state.props.rotate90 ? "h-0" : "h-auto"
-          } transition overflow-hidden`}
-        >
-          {tagsCount ? (
-            Object.keys(tagsCount).map((tag, index) => (
-              <Filter
-                rotate={state.props.rotate90}
-                info={{
-                  id: tags[index].id,
-                  name: tag,
-                  count: Object.values(tagsCount)[index],
-                }}
-                state={state}
-                dispatch={dispatch}
-                key={tag}
+          } `}>
+          <div className="hover:bg-primary-hover hover:rounded py-1 ">
+            <label htmlFor="reset" className="flex items-center cursor-pointer">
+              <img src={trash} alt="" className="!w-4 mr-4" />
+              Effacer les filtres
+            </label>
+            <input
+              type="reset"
+              name="reset"
+              id="reset"
+              hidden
+              onClick={() => tags.map((tag) => dispatch({ [tag.name]: false }))}
+            />
+          </div>
+          <div className="mt-4">
+            <span
+              className="flex items-center justify-between cursor-pointer"
+              onClick={() =>
+                dispatch({ props: { rotate90: !state.props.rotate90 } })
+              }
+            >
+              Spécialités
+              <img
+                src={arrow}
+                alt=""
+                className={`w-2 transition ${
+                  state.props.rotate90 ? "-rotate-90" : ""
+                }`}
               />
-            ))
-          ) : (
-            <Loader />
-          )}
-        </div>
-      </div>
-      <div className="mt-4">
-        <span
-          className="flex items-center justify-between cursor-pointer"
-          onClick={() =>
-            dispatch({ props: { rotate90: !state.props.rotate90 } })
-          }
-        >
-          Ville d'exercice 
-          <img
-            src={arrow}
-            alt=""
-            className={`w-2 transition ${
-              state.props.rotate90 ? "-rotate-90" : ""
-            }`}
-          />
-        </span>
-        <div
-          className={`${
-            state.props.rotate90 ? "h-0" : "h-auto"
-          } transition overflow-hidden`}
-        >
-          {tagsCount ? (
-            Object.keys(tagsCount).map((tag, index) => (
-              <Filter
-                rotate={state.props.rotate90}
-                info={{
-                  id: tags[index].id,
-                  name: tag,
-                  count: Object.values(tagsCount)[index],
-                }}
-                state={state}
-                dispatch={dispatch}
-                key={tag}
+            </span>
+            <div
+              className={`${
+                state.props.rotate90 ? "h-0" : "h-auto"
+              } transition overflow-hidden`}
+            >
+              {tagsCount ? (
+                Object.keys(tagsCount).map((tag, index) => (
+                  <Filter
+                    rotate={state.props.rotate90}
+                    info={{
+                      id: tags[index].id,
+                      name: tag,
+                      count: Object.values(tagsCount)[index],
+                    }}
+                    state={state}
+                    dispatch={dispatch}
+                    key={tag}
+                  />
+                ))
+              ) : (
+                <Loader />
+              )}
+            </div>
+          </div>
+          <div className="mt-4">
+            <span
+              className="flex items-center justify-between cursor-pointer"
+              onClick={() =>
+                dispatch({ props: { rotate90: !state.props.rotate90 } })
+              }
+            >
+              Ville d'exercice 
+              <img
+                src={arrow}
+                alt=""
+                className={`w-2 transition ${
+                  state.props.rotate90 ? "-rotate-90" : ""
+                }`}
               />
-            ))
-          ) : (
-            <Loader />
-          )}
+            </span>
+            <div
+              className={`${
+                state.props.rotate90 ? "h-0" : "h-auto"
+              } transition overflow-hidden`}
+            >
+              {tagsCount ? (
+                Object.keys(tagsCount).map((tag, index) => (
+                  <Filter
+                    rotate={state.props.rotate90}
+                    info={{
+                      id: tags[index].id,
+                      name: tag,
+                      count: Object.values(tagsCount)[index],
+                    }}
+                    state={state}
+                    dispatch={dispatch}
+                    key={tag}
+                  />
+                ))
+              ) : (
+                <Loader />
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+      
   );
 }
